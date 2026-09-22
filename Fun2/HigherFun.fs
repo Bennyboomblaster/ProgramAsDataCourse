7a817d0464d12586421779fb7e4e8150306be661
Jacob Folkmann <jafo@itu.dk>
2026-09-22 12:02:41 +0200

    add fun1 and fun2 solved 6.2

--- Fun2/HigherFun.fs
+++ Fun2/HigherFun.fs
@@ -30,6 +30,7 @@ let rec lookup env x =
 type value = 
   | Int of int
   | Closure of string * string * expr * value env       (* (f, x, fBody, fDeclEnv) *)
+  | Clos of string * expr * value env
 
 let rec eval (e : expr) (env : value env) : value =
     match e with
@@ -55,6 +56,7 @@ let rec eval (e : expr) (env : value env) : value =
       | Int 0 -> eval e3 env
       | Int _ -> eval e2 env
       | _     -> failwith "eval If"
+    | Fun(x,body) -> Clos(x, body, env)
     | Letfun(f, x, fBody, letBody) -> 
       let bodyEnv = (f, Closure(f, x, fBody, env)) :: env
       eval letBody bodyEnv
@@ -64,7 +66,11 @@ let rec eval (e : expr) (env : value env) : value =
       | Closure (f, x, fBody, fDeclEnv) ->
         let xVal = eval eArg env
         let fBodyEnv = (x, xVal) :: (f, fClosure) :: fDeclEnv
-        in eval fBody fBodyEnv
+        eval fBody fBodyEnv
+      | Clos (x, body, fDeclEnv) ->
+        let xVal = eval eArg env
+        let fBodyEnv = (x, xVal) :: fDeclEnv
+        eval body fBodyEnv 
       | _ -> failwith "eval Call: not a function";;
 
 (* Evaluate in empty environment: program must have no free variables: *)
